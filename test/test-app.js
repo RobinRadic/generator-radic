04e3fd21baa67391af1140d89ef8d0a15ee922f0
Robin Radic <robin@radic.nl>
2014-10-04 03:56:23 +0200

    small test addition and travis update

--- test/test-app.js
+++ test/test-app.js
@@ -7,22 +7,28 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('radic:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
-      .on('end', done);
-  });
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../app'))
+            .inDir(path.join(os.tmpdir(), './temp-test'))
+            .withOptions({'skip-install': true})
+            .withPrompt({
+                someOption: true
+            })
+            .on('end', done);
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
-  });
+    it('creates files', function () {
+        assert.file([
+            'bower.json',
+            'package.json',
+            '.editorconfig',
+            '.jshintrc',
+            'Gruntfile.js',
+            'README.md',
+            '.gitignore',
+            '.travis.yml',
+            'LICENSE',
+            'config.yml'
+        ]);
+    });
 });
