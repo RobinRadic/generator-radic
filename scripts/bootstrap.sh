60b0070bce16559f96ecf415a6de86433d74ed5a
robinradic <robin@radic.nl>
2015-01-22 11:34:33 +0100

    update

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,6 +5,7 @@ cd $sdir
 cd ..
 rdir=$PWD
 
-git submodule update --init --remote --force --recursive _includes
-cp scripts/pre-commit .git/hooks
+git submodule add https://github.com/robinradic/shared-gh-pages _includes
+#git submodule update --init --remote --force --recursive _includes
+cp scripts/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
